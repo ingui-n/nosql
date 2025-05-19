f9e01309cb1a153d0dcbd0e4d74202659f1f0c4c
ingui-n <inguin@email.cz>
2025-05-20 00:57:57 +0200

    improved visualization && added cluster schema

--- visualization/.ipynb_checkpoints/db_connection-checkpoint.py
+++ visualization/.ipynb_checkpoints/db_connection-checkpoint.py
@@ -14,3 +14,7 @@ def get_db_connection():
     mongo_url = f'mongodb://{mongo_user}:{mongo_password}@{mongo_01_ip}:{mongo_port1},{mongo_02_ip}:{mongo_port2}/{mongo_database}?authSource=admin'
     client = MongoClient(mongo_url)
     return client[mongo_database]
+
+def get_db_client():
+    mongo_url = f'mongodb://{mongo_user}:{mongo_password}@{mongo_01_ip}:{mongo_port1},{mongo_02_ip}:{mongo_port2}/{mongo_database}?authSource=admin'
+    return MongoClient(mongo_url)
